66ecbf7a54f0d5f9c25ea9a7fb7ce15ecfbfc5df
triplek(vm) <triplek@triplek.net>
2022-06-30 12:06:55 +0900

    5.0 My Own Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
 import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, where, orderBy } from "firebase/firestore";
 
-export default function Profile() {
+export default function Profile({ userObj }) {
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
   };
+
+  const getNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log({
+        ...doc.data(),
+        id: doc.id,
+      });
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   return (
     <div>
